956df42a420eff5aff57327e4abb17cb0fb182a1
Fatih Dogan <fatih.dogan@bitlc.onmicrosoft.com>
2024-08-20 01:14:01 +0200

    20.08

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,17 @@ import mysql from "mysql"
 
 const app=express()
 
+app.use(express.json())
+
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"12345",
+    password:"14785236",
     database:"test"
 })
 
 app.get("/",(req,res)=>{
-    res.json("hello this is the backend!")
+    res.json("hello this is the backendsdfsfd")
 })
 
 app.get("/books", (req,res)=>
@@ -23,6 +25,26 @@ app.get("/books", (req,res)=>
             return res.json(data)
 
     })
+})
+
+app.post("/books", (req,res)=>{
+    const q ="INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)"
+    const values =[
+
+        req.body.title,
+        req.body.desc,
+        req.body.cover
+        // "tittle from backend",
+        // "desc from backend",
+        // "cover from backend",
+    ];
+
+    db.query(q,[values],(err,data)=>{
+        if(err) return res.json(err) 
+            return res.json("book has been created")
+
+    })
+
 })
 
 app.listen(8800,()=>{
